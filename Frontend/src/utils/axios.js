a4fcad4c82ff997f1c36ce6748114cd95ffa4504
Krish18 <krishnataur018@gmail.com>
2025-12-15 02:20:16 +0530

    Prepare frontend for production API

--- Frontend/src/utils/axios.js
+++ Frontend/src/utils/axios.js
@@ -1,10 +1,9 @@
 import axios from "axios";
 import qs from "qs";
 
-// Serialization means converting a JavaScript object or array into a URL query string that can be sent in an HTTP request.
-
 export const axiosInstance = axios.create({
-  baseURL: "/api",
+  baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
-  paramsSerializer: (params) => qs.stringify(params, { arrayFormat: "repeat" }),
+  paramsSerializer: (params) =>
+    qs.stringify(params, { arrayFormat: "repeat" }),
 });
